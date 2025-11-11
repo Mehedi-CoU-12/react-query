4e2106b8184a237f95162732290ef3df7bb28b6e
mehedi_cou_12 <mehedihasana383@gmail.com>
2025-11-11 19:00:01 +0600

    refactor: integrate tag fetching and post creation functionality

--- src/components/Post_list.jsx
+++ src/components/Post_list.jsx
@@ -1,6 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import React from "react";
-import { fetchPosts } from "../api/api";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { addPost, fetchPosts, fetchTags } from "../api/api";
 
 function Post_list() {
     const {
@@ -13,55 +12,113 @@ function Post_list() {
         queryFn: fetchPosts,
     });
 
-    console.log("---data", postData, isLoading, isError, error);
+    const {
+        data: tagData,
+        isLoading: isTagLoading,
+        isError: isTagError,
+        error: tagError,
+    } = useQuery({
+        queryKey: ["tags"],
+        queryFn: fetchTags,
+    });
+
+    const {
+        mutate,
+        isError: isPostError,
+        isPending,
+        error: postError,
+        reset,
+    } = useMutation({
+        queryFn: addPost,
+    });
+
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const title = formData.get("title");
+        const tags = Array.from(formData.keys()).filter(
+            (key) => formData.get(key) === "on"
+        );
+
+        if (!title || !tags) return;
+
+        mutate({ id: postData.length + 1, title, tags });
+
+        e.target.reset();
+    };
+
+    if (isLoading || isTagLoading) return <div>Loading....</div>;
 
     return (
         <>
-            {isLoading && <div>Loading...</div>}
-            {isError && <div>{error?.message}</div>}
-            {postData && (
-                <div>
-                    {postData.map((post) => {
-                        return (
-                            <div
-                                style={{
-                                    marginBottom: "20px",
-                                    padding: "10px",
-                                    border: "1px solid black",
-                                }}
-                                key={post.id}
-                            >
-                                <span
+            <div>
+                {isTagLoading && <div>Loading...</div>}
+                {isTagError && <div>{tagError?.message}</div>}
+                <form onSubmit={handleClick}>
+                    <input
+                        type="text"
+                        placeholder="enter your post..."
+                        name="title"
+                    />
+                    <div>
+                        {tagData.map((tag) => (
+                            <div key={tag}>
+                                <input type="checkbox" id={tag} name={tag} />
+                                <label htmlFor={tag}>{tag}</label>
+                            </div>
+                        ))}
+                    </div>
+                    <button style={{ marginTop: "10px" }}>Create Post</button>
+                </form>
+            </div>
+            <h1>Post List</h1>
+            <div>
+                {isLoading && <div>Loading...</div>}
+                {isError && <div>{error?.message}</div>}
+                {postData && (
+                    <div>
+                        {postData.map((post) => {
+                            return (
+                                <div
                                     style={{
-                                        textAlign: "center",
-                                        fontSize: "30px",
-                                        fontWeight: "semi-bold",
-                                        marginRight: "10px",
+                                        marginBottom: "20px",
+                                        padding: "10px",
                                     }}
+                                    key={post.id}
                                 >
-                                    {post.title}
-                                </span>
-
-                                {post.tags.map((tag) => (
                                     <span
                                         style={{
                                             textAlign: "center",
-                                            background: "grey",
-                                            color: "white",
-                                            padding: "2px 5px",
-                                            borderRadius: "5px",
+                                            fontSize: "30px",
+                                            fontWeight: "semi-bold",
                                             marginRight: "10px",
                                         }}
-                                        key={tag}
                                     >
-                                        {tag}
+                                        {post.title}
                                     </span>
-                                ))}
-                            </div>
-                        );
-                    })}
-                </div>
-            )}
+
+                                    {post.tags.map((tag) => (
+                                        <span
+                                            style={{
+                                                textAlign: "center",
+                                                background: "grey",
+                                                color: "white",
+                                                padding: "2px 5px",
+                                                borderRadius: "5px",
+                                                marginRight: "10px",
+                                            }}
+                                            key={tag}
+                                        >
+                                            {tag}
+                                        </span>
+                                    ))}
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
         </>
     );
 }
